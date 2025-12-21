87547ff7afcd0ae26605aec2cf370419b806c496
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2025-12-21 05:41:57 -0600

    chore: update copilot config

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,1 +1,10 @@
-return { "github/copilot.vim" }
+return {
+	"github/copilot.vim",
+	config = function()
+		vim.api.nvim_create_user_command("Silencio", "Copilot disable", {})
+    vim.api.nvim_create_user_command("Ruido", "Copilot enable", {})
+
+    vim.keymap.set('n', '<leader>cs', ':Silencio<CR>', { desc = "Disable Copilot" })
+    vim.keymap.set('n', '<leader>cr', ':Ruido<CR>', { desc = "Enable Copilot" })
+	end,
+}
