12e8b4211f603b9b83110349152c7ef1563bb49d
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2025-12-22 17:00:38 -0600

    feat: add debugger for Go

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -244,9 +244,52 @@ return {
 			},
 		}
 
-		local codelldb_path = vim.fn.has("win32") 
-      and vim.fn.stdpath("data") .. "/mason/packages/codelldb/extension/adapter/codelldb.exe" 
-      or vim.fn.stdpath("data") .. "/mason/packages/codelldb/extension/adapter/codelldb"
+		dap.adapters.delve = function(callback, config)
+			if config.mode == "remote" and config.request == "attach" then
+				callback({
+					type = "server",
+					host = config.host or "127.0.0.1",
+					port = config.port or "38697",
+				})
+			else
+				callback({
+					type = "server",
+					port = "${port}",
+					executable = {
+						command = "dlv",
+						args = { "dap", "-l", "127.0.0.1:${port}", "--log", "--log-output=dap" },
+						detached = vim.fn.has("win32") == 0,
+					},
+				})
+			end
+		end
+
+		dap.configurations.go = {
+			{
+				type = "delve",
+				name = "Debug",
+				request = "launch",
+				program = "${file}",
+			},
+			{
+				type = "delve",
+				name = "Debug test",
+				request = "launch",
+				mode = "test",
+				program = "${file}",
+			},
+			{
+				type = "delve",
+				name = "Debug test (go.mod)",
+				request = "launch",
+				mode = "test",
+				program = "./${relativeFileDirname}",
+			},
+		}
+
+		local codelldb_path = vim.fn.has("win32")
+				and vim.fn.stdpath("data") .. "/mason/packages/codelldb/extension/adapter/codelldb.exe"
+			or vim.fn.stdpath("data") .. "/mason/packages/codelldb/extension/adapter/codelldb"
 
 		dap.adapters.lldb = {
 			type = "server",
