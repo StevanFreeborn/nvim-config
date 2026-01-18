503fa008495c57a2211dfca39f5e9d15c14eb5fe
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2026-01-18 04:40:15 -0600

    feat: add keys config

---
+++ lua/plugins/keys.lua
@@ -0,0 +1,11 @@
+return {
+  "tamton-aquib/keys.nvim",
+  config = function()
+    require("keys").setup({
+      enable_on_startup = true,
+      win_opts = {
+        width = 25,
+      },
+    })
+  end,
+}
