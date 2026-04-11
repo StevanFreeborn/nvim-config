e7659a6066ed364c7429ab8479811a1e94fb6fc3
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2026-04-11 14:51:56 -0500

    feat: customize path display

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,20 +6,20 @@ return {
 		config = function()
 			local builtin = require("telescope.builtin")
 			local action_state = require("telescope.actions.state")
-      local actions = require("telescope.actions")
+			local actions = require("telescope.actions")
 
 			vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Telescope find files" })
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 
 			Buffer_searcher = function()
 				builtin.buffers({
-          initial_mode = "normal",
+					initial_mode = "normal",
 					sort_mru = true,
 					attach_mappings = function(prompt_buffer, map)
-            local refresh_buffer_searcher = function()
-                actions.close(prompt_buffer)
-                vim.schedule(Buffer_searcher)
-            end
+						local refresh_buffer_searcher = function()
+							actions.close(prompt_buffer)
+							vim.schedule(Buffer_searcher)
+						end
 
 						local delete_buf = function()
 							local picker = action_state.get_current_picker(prompt_buffer)
@@ -27,7 +27,7 @@ return {
 							for _, entry in ipairs(selection) do
 								vim.api.nvim_buf_delete(entry.bufnr, { force = true })
 							end
-              refresh_buffer_searcher()
+							refresh_buffer_searcher()
 						end
 
 						map("n", "dd", delete_buf)
@@ -46,6 +46,12 @@ return {
 		"nvim-telescope/telescope-ui-select.nvim",
 		config = function()
 			require("telescope").setup({
+				defaults = {
+					path_display = function(_, path)
+						local tail = require("telescope.utils").path_tail(path)
+						return string.format("%s (%s)", tail, path)
+					end,
+				},
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown({}),
