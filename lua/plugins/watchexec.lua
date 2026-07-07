3713cd68b96ac2d1c4750549c488b3bd528ad48e
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2026-07-06 20:52:53 -0500

    feat: update config

--- lua/plugins/watchexec.lua
+++ lua/plugins/watchexec.lua
@@ -1,8 +1,14 @@
 return {
-  "StevanFreeborn/watchexec.nvim",
-  lazy = false,
-  config = function()
-    vim.api.nvim_set_hl(0, "WatchexecSuccess", { bg = "#7ec8c0" })
-    vim.api.nvim_set_hl(0, "WatchexecFailure", { bg = "#d47373" })
-  end,
+	"StevanFreeborn/watchexec.nvim",
+	lazy = false,
+	config = function()
+		require("watchexec").setup({
+			indicator = {
+				padding = { x = 2, y = 3 },
+			},
+		})
+
+		vim.api.nvim_set_hl(0, "WatchexecSuccess", { bg = "#7ec8c0" })
+		vim.api.nvim_set_hl(0, "WatchexecFailure", { bg = "#d47373" })
+	end,
 }
